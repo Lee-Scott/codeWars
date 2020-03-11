7e903a15805e29583c07ca85fbeba178ddfcf068
Lee-Scott <37646873+Lee-Scott@users.noreply.github.com>
2020-03-11 16:10:58 -0500

    Initial commit

---
+++ src/reversePolishNotationCalculator/Calc.java
@@ -0,0 +1,29 @@
+package reversePolishNotationCalculator;
+
+import java.util.Stack;
+
+public class Calc {
+
+    public double evaluate(String s) {
+        if ("".equals(s)) {
+            return 0;
+        }
+
+        Stack<Double> stack = new Stack<Double>();
+        String[] exp = s.split(" ");
+
+        for (String singleExp: exp) {
+            Double x, y;
+            switch (singleExp) {
+                case "+": stack.push(stack.pop() + stack.pop()); break;
+                case "-": y = stack.pop(); x = stack.pop(); stack.push(x - y); break;
+                case "*": stack.push(stack.pop() * stack.pop()); break;
+                case "/": y = stack.pop(); x = stack.pop(); stack.push(x / y); break;
+                default:
+                    stack.push(Double.parseDouble(singleExp));
+            }
+        }
+
+        return stack.pop();
+    }
+}
