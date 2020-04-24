04a958759dec939c9a4061514dae1f100144a238
Lee-Scott <Github4934506>
2020-04-24 09:55:16 -0500

    Still cleaning up BinaryTree code....

--- src/nodes/Test.java
+++ src/nodes/Test.java
@@ -2,13 +2,5 @@ package nodes;
 
 public class Test {
 
-    @Test
-    public void givenABinaryTree_WhenAddingElements_ThenTreeContainsThoseElements() {
-        BinaryTree bt = createBinaryTree();
-
-        assertTrue(bt.containsNode(6));
-        assertTrue(bt.containsNode(4));
-
-        assertFalse(bt.containsNode(1));
-    }
 }
+
