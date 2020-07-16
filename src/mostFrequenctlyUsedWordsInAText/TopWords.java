f46f3bf6369e2c574b5f1efffc63aa4c2b40e5e4
Lee-Scott <37646873+Lee-Scott@users.noreply.github.com>
2020-07-16 11:30:58 -0400

    CodeWar  7/16/20 the Millionth Fibonacci

---
+++ src/mostFrequenctlyUsedWordsInAText/TopWords.java
@@ -0,0 +1,15 @@
+package mostFrequenctlyUsedWordsInAText;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class TopWords {
+
+    final static private Pattern P = Pattern.compile("[a-z][a-z']*");
+    
+    public static List<String> top3(String s) {
+
+
+
+        return null;
+    }
+}
---
+++ src/theMillonthFibonacci/Fibonacci.java
@@ -0,0 +1,24 @@
+package theMillonthFibonacci;
+
+import java.math.BigInteger;
+
+public class Fibonacci {
+    public static final BigInteger ZERO = BigInteger.valueOf(0);
+    public static final BigInteger ONE  = BigInteger.valueOf(1);
+    public static final BigInteger TWO  = BigInteger.valueOf(2);
+
+    public static BigInteger fib(BigInteger num) {
+        BigInteger result = fibNum(ONE, ZERO, ZERO, ONE, num.abs());
+        return (num.signum() < 0) && num.mod(TWO).equals(ZERO) ? result.negate() : result;
+    }
+
+    public static BigInteger fibNum(BigInteger a, BigInteger b, BigInteger c, BigInteger d, BigInteger n) {
+        if (n.equals(ZERO))
+            return b;
+        if (n.mod(TWO).equals(ZERO))
+            return fibNum(a, b, c.pow(2).add(d.pow(2)), c.multiply(d.multiply(TWO)).add(d.pow(2)), n.divide(TWO));
+        else
+            return fibNum(b.multiply(d).add(a.multiply(d)).add(a.multiply(c)), b.multiply(c).add(a.multiply(d)), c, d, n.subtract(ONE));
+    }
+
+}
